7407c0a96ec27d39df789cda9e1f3a788a593124
lumusitech <lumusitech@gmail.com>
2021-11-17 19:28:30 -0300

    [ADD] template form - switches added

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
+  person = {
+    genre: 'F',
+    notifications: true
+  };
 
-  ngOnInit(): void {
-  }
-
+  termsAndConditions: boolean = false;
 }
