bcf7a5a8ddf4b9fdf2d2575f938aad6ae29dd29b
lumusitech <lumusitech@gmail.com>
2021-11-18 14:06:44 -0300

    [ADD] reactive form - switches added

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    genre: ['M', Validators.required],
+    notifications: [true, Validators.required],
+    termsAndConditions: [true, Validators.requiredTrue]
+  });
 
-  ngOnInit(): void {
+  person = {
+    genre: 'F',
+    notifications: true
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm.reset({ ...this.person, termsAndConditions: false });
+
+
+    // en el caso de que se necesite actualizar a el objeto person ante cualquier cambio
+    this.myForm.valueChanges.subscribe(({ termsAndConditions, ...rest }) => {
+      this.person = rest;
+      // console.log(rest);
+    });
+
+    // En el caso de que se necesite actualizar al cambiar un control específico
+    // this.myForm.get('termsAndConditions')?.valueChanges.subscribe(terms => {
+    //   console.log(terms);
+    // });
+  }
+
+  save() {
+    if (this.myForm.invalid) return;
+
+    const formValues = { ...this.myForm.value };
+    delete formValues.termsAndConditions
+
+    console.log(formValues);
+
+    this.person = formValues;
+
+    this.myForm.reset();
+
   }
 
 }
