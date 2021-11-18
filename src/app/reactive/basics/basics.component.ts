5358c9111c586de5b6d44b6af1064c7483a1f431
lumusitech <lumusitech@gmail.com>
2021-11-17 23:27:39 -0300

    [ADD] reactive form - basics added

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -6,11 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent implements OnInit{
 
-  constructor() { }
+  // Opción más verbosa:
+  // myForm: FormGroup = new FormGroup({
+  //   name : new FormControl('RTX 4080ti'),
+  //   price: new FormControl(1000),
+  //   stock: new FormControl(5)
+  // });
 
-  ngOnInit(): void {
+  myForm: FormGroup = this.fb.group({
+    name: [, [Validators.required, Validators.minLength(3)]],
+    price: [, [Validators.required, Validators.min(0)]],
+    stock: [, [Validators.required, Validators.min(0)]],
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(){
+    // to set defaults values on init
+    // this.myForm.setValue({
+    //   name: 'RTX 4080ti',
+    //   price: 1000,
+    //   stock: 2
+    // });
   }
 
+  isInvalidField(field: string) {
+    return this.myForm.controls[field]?.invalid &&
+      this.myForm.controls[field]?.touched
+  }
+
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
